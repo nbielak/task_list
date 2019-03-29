4485f0d9e91d64e00b22363edb95f73888ba83d2
your name here <nickbielak95@gmail.com>
2019-03-28 22:09:07 -0700

    Updates

---
+++ precache-manifest.4739d87f3875b4452242f5a4485f1d33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdd500172fe24e927c65bc0daa402d36",
+    "url": "/task_list/static/media/Locked.fdd50017.svg"
+  },
+  {
+    "revision": "274a394e747750f24486989a0b2cfc2e",
+    "url": "/task_list/static/media/Incomplete.274a394e.svg"
+  },
+  {
+    "revision": "eec740fed6e3cd4a20091ed3bfe37cee",
+    "url": "/task_list/static/media/Group.eec740fe.svg"
+  },
+  {
+    "revision": "4230b7aa678fc3860541b9aaf6ab20f5",
+    "url": "/task_list/static/media/Completed.4230b7aa.svg"
+  },
+  {
+    "revision": "d52895c2ac488f7bfd9d",
+    "url": "/task_list/static/js/runtime~main.fe9be80e.js"
+  },
+  {
+    "revision": "3e3237e540d3f08f2f74",
+    "url": "/task_list/static/js/main.44a8ed38.chunk.js"
+  },
+  {
+    "revision": "5fd1a57e25464696cd38",
+    "url": "/task_list/static/js/2.f78c6fe6.chunk.js"
+  },
+  {
+    "revision": "8855168e767dffe1dcc8365f4b32b95b",
+    "url": "/task_list/index.html"
+  }
+];
